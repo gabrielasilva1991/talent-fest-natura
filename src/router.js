84c9629aba830b494940dfb812ef13acc87a420e
gabrielasilva1991 <gabriela.silva1991@outlook.com>
2021-04-14 14:56:22 -0300

    correção de arquivos para uso dentro dos componentes input radio

--- src/router.js
+++ src/router.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home/index';
-import Login from './pages/login/index';
+// import Login from './pages/login/index';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path ='/' component={Home} exact />
-        <Route path ='/login' component={Login} exact />
+        {/* <Route path ='/login' component={Login} exact /> */}
       </Switch>
     </BrowserRouter>
   )
