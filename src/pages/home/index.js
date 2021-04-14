c0e6259676144f6891db6811f112e74f79e224c9
akemimeka <akemimeka@gmail.com>
2021-04-14 05:08:09 -0300

    Cria HTML base para a Home, dividindo em sections

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,23 +1,71 @@
 import React from 'react';
+import photoExample01 from '../../img/boa-foto-01.jpg';
+import photoExample03 from '../../img/boa-foto-03.jpg';
 
-//inserido {Link} para teste de rotas
-import { Link } from 'react-router-dom';
-
-const Home = () => {
- 
+export default function Home() {
   return (
     <>
-      <div className='page-container'>
+      <header className='header centered'>
+        <h1 className='find-your-red'>Descubra o<br />
+          <span className='handwritten-font'>seu vermelho</span>
+        </h1>
 
-        <h1>Home</h1>
+        <a href='#section-name'>
+          <button className='nav-button'>
+            <p className='nav-button-text'>Próximo</p>
+            <i className='icon-arrow-down fas fa-chevron-down'></i>
+          </button>
+        </a>
+      </header>
 
-        <p>
-          <Link to='/login'>Login</Link>
-        </p>
-        
-      </div>
+      <main>
+        <section id='section-name' className='section-base centered'>
+          <label className='default-text'>Insira seu nome</label>
+          <input
+            required
+            type='text'
+            minLength='1'
+            size='10'
+            className='input-username centered'
+          />
+        </section>
+
+        <section id='section-photo' className='section-photo section-base centered'>
+          <p>Faça o upload de uma foto sua.</p>
+          <p>É importante que a foto seja de rosto, com ele ocupando quase toda a imagem.</p>
+          <p>Exemplos:</p>
+          <div className='photo-examples-wrap'>
+            <img
+              src={photoExample01}
+              alt='Rosto de uma mulher caucasiana, loira, sorrindo e olhando para a câmera.'
+              className='photo-example'
+            ></img>
+            <img
+              src={photoExample03}
+              alt='Rosto de uma mulher negra, sorrindo e olhando para seu lado esquerdo.'
+              className='photo-example'
+            ></img>
+
+          </div>
+          <button className='button-base'>Enviar foto</button>
+        </section>
+
+        <section id='section-quiz-01' className='section-base centered'>
+          <p className='default-text'>1. Que tipo de vermelho mais combina com você?</p>
+          <button className='button-base'>Aberto e alegre</button>
+          <button className='button-base'>Clássico e atemporal</button>
+          <button className='button-base'>Arroxeado e vibrante</button>
+        </section>
+
+        <section id='section-manifesto' className='section-base centered'>
+        </section>
+
+        <section id='section-reveal' className='section-base centered'>
+        </section>
+
+        <section id='section-lipstick' className='section-base centered'>
+        </section>
+      </main>
     </>
   );
 }
-
-export default Home;
