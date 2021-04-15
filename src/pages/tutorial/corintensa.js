042837bb7c5eb1da52f40a6c15c148fb36a91ef5
Carina Rocha <criasdacaz@gmail.com>
2021-04-15 14:40:48 -0300

    Correção nas rotas

--- src/pages/tutorial/corintensa.js
+++ src/pages/tutorial/corintensa.js
@@ -4,7 +4,7 @@ export default function TutorialCorIntensa (){
   return(
     <header className='header centered'>
     <div className='header-container'>
-      <h1><br />
+      <h1>A cor não fica intensa<br />
         <p className='lipstick-description'>Isso acontece porque o tom natural dos lábios pode interferir na cor original do batom, 
         <br />o  segredo para conquistar uma nuance bem fiel à do produto é hidratar e depois do cobrir os lábios com o corretivo ou a base de costume.</p>
       </h1>
