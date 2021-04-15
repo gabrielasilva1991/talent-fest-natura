738821a03d693f47d25e0d907a9b4329820811d6
Carina Rocha <criasdacaz@gmail.com>
2021-04-15 14:25:07 -0300

    Tutoriais inseridos

--- src/pages/tutorial/bocapeq.js
+++ src/pages/tutorial/bocapeq.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function TutorialBocaPeq (){
   return(
-    <h1>Tutorial</h1>
+    <header className='header centered'>
+      <div className='header-container'>
+        <h1>Tenho lábios finos<br />
+          <p className='lipstick-description'>Para conquistar uma aplicação mais precisa respeitando o desenho natural dos seus lábios, 
+          <br />comece contornando a boca com um lápis para lábios da mesma cor do batom ou em um tom o mais próximo possível.</p>
+        </h1>
+      </div>
+    </header>
   )
 };
--- src/pages/tutorial/borrafacil.js
+++ src/pages/tutorial/borrafacil.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function TutorialBorra (){
   return(
-    <h1>Tutorial</h1>
+    <header className='header centered'>
+    <div className='header-container'>
+      <h1>Borra com Facilidade<br />
+        <p className='lipstick-description'>Borrou? Não tem problema! Dá pra salvar o make sem ficar com resquícios do vermelho borrado. 
+        <br />Você pode usar um cotonete ou o pincel de corretivo com um pouco da base que você usou na maquiagem. É só aplicar ao redor da boca  e nas áreas borradas para dar aquele acabamento perfeito</p>
+      </h1>
+    </div>
+  </header>
   )
 };
